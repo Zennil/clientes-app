a7b0d980a73d433e27733dbd37bd478ca32ac003
Armando Zenil <szenil.11@gmail.com>
2020-03-18 13:31:21 -0600

    Se implementa Observable en el servicio de Cliente

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
-
+import { Observable, of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,6 +9,8 @@ export class ClienteService {
 
   constructor() { }
 
-  getClientes(): Cliente[] { return CLIENTES; }
+  getClientes(): Observable<Cliente[]> {
+    return of(CLIENTES);
+  }
 
 }
