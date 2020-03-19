8ed8c2237257ff43ec950910c03ffeec191d57e6
Armando Zenil <szenil.11@gmail.com>
2020-03-19 15:39:43 -0600

    Se instala Sweetalert2 al proyecto

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -21,7 +22,10 @@ export class FormComponent implements OnInit {
   create(): void {
     console.log('Clicked');
     console.log(this.cliente);
-    this.clienteService.create(this.cliente).subscribe(reponse => this.router.navigate(['/clientes']));
+    this.clienteService.create(this.cliente).subscribe(reponse => {
+      this.router.navigate(['/clientes']);
+      Swal.fire('Nuevo cliente', `Cliente ${reponse.nombre} creado con exito.`, 'success');
+    });
   }
 
 }
