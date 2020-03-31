877842fdc87148ee14e9f18fa35b65dacaeae036
Armando Zenil <szenil.11@gmail.com>
2020-03-31 16:05:29 -0600

    Se agrega el metodo login() en el componente y una entidad Usuario

--- src/app/usuarios/login.component.ts
+++ src/app/usuarios/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from './usuario';
+import Swal, { } from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   titulo = 'Por favor Inicia Sesion';
+  usuario: Usuario;
 
-  constructor() { }
+
+  constructor() {
+    this.usuario = new Usuario();
+  }
 
   ngOnInit() {
+  }
+
+  login(): void {
+    console.log(this.usuario);
+    if (!this.usuario.username || !this.usuario.password) {
+      Swal.fire('Error Login', 'Username o password vacías!', 'error');
+      return;
+    }
   }
 
 }
